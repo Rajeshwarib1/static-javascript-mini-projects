adfbda181ed875f028a54ca9361b6ca610b81235
Rajeshwarib1 <rajeshwaribhalerao95@gmail.com>
2024-12-12 20:02:57 +0530

    updates

--- 02_BMI_Calculator/script.js
+++ 02_BMI_Calculator/script.js
@@ -1,18 +1,18 @@
-const forms = document.querySelector('form')
+const form = document.querySelector('form')
 
-forms.addEventListener('submit', function(e){
-    e.preventDefault()
+form.addEventListener('submit', function(e){
+    e.preventDefault();
 
-    const height = parseInt(document.querySelector('#height').value)
-    const weight = parseInt(document.querySelector('#weight').value)
-    const results = document.querySelector('#result')
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
 
     if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML =` please give a valid height ${height}`
     } else if(weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML =` please give a valid height ${weight}`
     } else {
-        (weight / ((height * height) / 1000)).toFixed(2)
+        const bmi = (weight / ((height * height) / 1000)).toFixed(2)
         //show result
         results.innerHTML = `<span>${bmi}</span>`
     }
